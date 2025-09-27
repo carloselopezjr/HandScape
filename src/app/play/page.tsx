6ff78aff5ab2c4a8d129a459584d8bad3b665767
Carlos <lopez212318@gmail.com>
2025-09-27 03:18:17 -0400

    Dynamic camera orientation based on selected cube, color changes

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -3,7 +3,7 @@
 import ThreeJS from '../components/threejs/3dtesting';
 export default function Play() {
     return (
-        <div className="bg-gray-500">
+        <div className="">
             <div className="flex justify-center">
                 <ThreeJS />
             </div>
